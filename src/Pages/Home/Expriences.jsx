5bc588d2f83ff69b2a957d4e0ff4856c67794ff9
Naofel-Badsha <naofelbadsha@gmail.com>
2025-11-19 06:09:09 +0600

    added experiences  section

---
+++ src/Pages/Home/Expriences.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import expriencesImages from "../../assets/expricences.png";
+import Button from "./Button";
+const Expriences = () => {
+  return (
+    <section className="section-container my-24">
+      <div className="flex flex-col md:flex-row gap-10 items-center justify-between">
+        <div className="md:w-1/2 md:h-[541px] flex-1">
+          <img src={expriencesImages} alt="" className="h-full w-full" />
+        </div>
+        <div className="flex-1">
+          <h3 className="text-xl text-[#FFA704] font-semibold mb-4">EXPERIENCES </h3>
+          <h1 className="text-5xl font-bold">
+            We Provide You The Best Experience
+          </h1>
+          <p className="text-[18px] mt-5">
+            You dorit have to wurry utxa the seat bosiusex Се Interiors are moda
+            by people who sans protasines in then froids with on ologant and
+            lucudous style and with premium quality materia's
+          </p>
+          <Button text="More Info" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Expriences;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './Hero';
 import WhayChoose from './WhayChoose';
 import Products from '../Shop/Products';
+import Expriences from './Expriences';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
            <Hero /> 
            <WhayChoose />
            <Products headline="Best Selling Product"/>
+           <Expriences />
         </>
     );
 };
